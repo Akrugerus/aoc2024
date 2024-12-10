f8351d564f328470c5b9b73026359d61d7ef69b0
Tony Krueger <tony@ntp-inc.com>
2024-12-10 21:32:27 +0000

    finish day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -3,17 +3,30 @@ import re
 
 class Runner:
     def __init__(self, data):
-        self.lines = data.split()
+        # The question means for us to interpret all lines as one
+        # and \n characters shouldn't influence the outcome
+        self.lines = [data]
 
     def get_mult_total(self):
         total = 0
         for line in self.lines:
-            for a in re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", line):
-                one, two = a[4:-1].split(",")
-                total += int(one) * int(two)
-                print(a)
-                print(one)
-                print(two)
+            total += self.get_mul_in_line(line)
+        return total
+
+    def get_mul_in_line(self, line):
+        total = 0
+        for a in re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", line):
+            one, two = a[4:-1].split(",")
+            print(one, two)
+            total += int(one) * int(two)
+        return total
+
+    def get_do_dont_mult_total(self):
+        total = 0
+        for line in self.lines:
+            for do_dont in line.split("do()"):
+                do = do_dont.split("don't()")[0]
+                total += self.get_mul_in_line(do)
         return total
 
 
@@ -22,6 +35,11 @@ if __name__ == "__main__":
         runner = Runner(file.read())
         assert runner.get_mult_total() == 161
 
+    with open("day3/sample2.txt", "r") as file:
+        runner = Runner(file.read())
+        assert runner.get_do_dont_mult_total() == 48
+
     with open("day3/input.txt", "r") as file:
         runner = Runner(file.read())
         print(runner.get_mult_total())
+        print(runner.get_do_dont_mult_total())
