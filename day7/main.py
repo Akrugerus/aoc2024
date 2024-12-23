4f61a6699e3aea64861476957415ae992fa66562
Tony Krueger <tony@ntp-inc.com>
2024-12-23 21:08:18 +0000

    finished day7 part2
    very slow because i just brute forced trying every permutation
    definitely looking forward to seeing other solutions to see how dumb i am

--- day7/main.py
+++ day7/main.py
@@ -6,7 +6,15 @@ class Runner:
         data = data.splitlines()
         self.lines = [line.split(":") for line in data]
 
-    def run(self):
+    def solve(self, left, op, right):
+        if op == "+":
+            return left + right
+        if op == "*":
+            return left * right
+        if op == "||":
+            return int(str(left) + str(right))
+
+    def run(self, *operators):
         total = 0
         print(self.lines)
         for line in self.lines:
@@ -14,11 +22,13 @@ class Runner:
             answer = int(answer.strip())
             parts = parts.strip().split()
             num_parts = len(parts)
-            perms = product(["*", "+"], repeat=num_parts - 1)
+            perms = product(operators, repeat=num_parts - 1)
             for perm in perms:
-                local_total = parts[0]
+                local_total = int(parts[0])
                 for idx in range(len(perm)):
-                    local_total = eval(f"{local_total} {perm[idx]} {parts[idx+1]}")
+                    local_total = self.solve(
+                        local_total, perm[idx], int(parts[idx + 1])
+                    )
 
                 if local_total == answer:
                     total += int(answer)
@@ -30,7 +40,9 @@ class Runner:
 if __name__ == "__main__":
     with open("day7/sample.txt") as file:
         runner = Runner(file.read())
-        assert runner.run() == 3749
+        assert runner.run("*", "+") == 3749
+        assert runner.run("*", "+", "||") == 11387
     with open("day7/input.txt") as file:
         runner = Runner(file.read())
-        print(runner.run())
+        print(runner.run("*", "+"))
+        print(runner.run("*", "+", "||"))
